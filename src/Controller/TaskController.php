c10cbded135a28b607160545bcf3d72bfbf3024c
lvisana <lsmm2705@gmail.com>
2023-04-25 18:32:11 -0400

    Base de datos y relaciones ORM

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use App\Repository\TasksRepository;
+
+class TaskController extends AbstractController
+{
+    #[Route('/task', name: 'app_task')]
+    public function index(TasksRepository $tasksRepository): Response
+    {
+        $tasks = $tasksRepository->findAll();
+
+        foreach ($tasks as $task) {
+            $userName = $task->getUser()->getName();
+            var_dump($userName);
+            echo '<br>';
+            echo '<br>';
+        }
+
+die();
+        return $this->render('task/index.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+}
